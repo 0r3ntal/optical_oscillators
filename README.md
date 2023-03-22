ba176e4d25f2f29b5563bcfb62eab70f84892f95
o_bac <oren.sternberg@icloud.com>
2023-03-22 14:59:57 -0700

    cleaned a bit

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Optical Oscillators V0.1 
+# Optical Oscillators Transmission Line Theory
 
-various scripts to simulate optical oscillators using transmission line theory
+This repository contains various scripts to simulate optical oscillators using transmission line theory.
 
-References: 
+## References: 
 1. [R. Ulrich (Mar 1967). "Far infrared properties of metallic mesh and its complementary structure". Infrared Physics. 7 (1): 37–50.](https://www.sciencedirect.com/science/article/abs/pii/0020089167900280)
 2. [Sternberg, Oren. Resonances of periodic metal-dielectric structures at the infrared wavelength region. New Jersey Institute of Technology, 2002](https://digitalcommons.njit.edu/dissertations/548/)
 
@@ -11,11 +11,11 @@ References:
 ### Dependencies
 * see [requirements.txt](https://github.com/0r3ntal/optical_oscillators/blob/main/requirements.txt) for python libraries
 
-
-
 ### Contact information
 
-emaiil: oren.sternberg@icloud.com
+email: oren.sternberg@icloud.com
+
+
 
 
 
--- opticaloscillator.py
+++ opticaloscillator.py
@@ -11,55 +11,28 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
 
-class Oscillator(object):
+class Oscillator:
     """
-    This class will calculate a set of N+2 inductive meshes
+    This class will calculate a set of N+2 inductive meshes.
     """
 
-    def __init__(self,a1,A1, g, l0):
+    def __init__(self, a1, A1, g, l0):
         self.g = g  # periodicity metric units
-        self._w = np.linspace(0.005, 0.1, 159)
+        self.w = np.linspace(0.005, 0.1, 159)
         self.l0 = l0
         self.w23 = self.g / self.l0
         self.a1 = a1
         self.A1 = A1
-        self.config = True # True normal oscillator, False Inverted oscillator
-
+        self.config = True  # True normal oscillator, False Inverted oscillator
         self.n1 = 1.0  # refractive index n1
         self.n2 = 1.0  # refractive index n2
         self.n3 = 1.0  # refractive index n2
-
         self.omega23 = (self.w * self.g / self.w23) - (self.w23 / (self.g * self.w))  # generalized frequency
-
-
-
-        # init matrices
-
-    @property
-    def w(self):
-        return self._w
-
-    @w.setter
-    def w(self, value):
-        self._w = value
-
-    @property
-    def config(self):
-        return self._config
-
-    @config.setter
-    def config(self, value):
-        self._config = value
-
 
     def matrixElementsM3(self):
         """
-
         Matrix for mesh from index n1 to n2 M12
-
-        :return:
         """
-
         if self.config:
             self.Y23 = 1 / (self.a1 - (1j * self.w23 * self.A1) / (self.omega23))  # normalized admittance
         else:
@@ -73,14 +46,12 @@ class Oscillator(object):
         self.M3 = np.array([[M311, M312], [M321, M322]], dtype=complex, order='F')
 
     def OscillatorPhaseGroupDelayDispersion(self):
-
-        self.phi=(180/np.pi)*np.arctan(np.imag(self.tf1)/np.real(self.tf1))
-        self.group_delay=np.gradient(self.phi,self.w)
-        self.dispersion = np.gradient(self.group_delay,self.w)
-
+        self.phi = (180 / np.pi) * np.arctan(np.imag(self.tf1) / np.real(self.tf1))
+        self.group_delay = np.gradient(self.phi, self.w)
+        self.dispersion = np.gradient(self.group_delay, self.w)
 
     def filteroutput(self):
-        print('A1       ->{}'.format(self.A1))
+        print('A1 -> {}'.format(self.A1))
         self.matrixElementsM3()
         self.tf1 = 1 / self.M3[1, 1]
         self.absTF1 = np.abs(self.tf1)
@@ -89,65 +60,56 @@ class Oscillator(object):
 
 class MixingOscillators(object):
     """
-    class to take two oscillators in different planes (treat as uncoupled)
-    and mix amplitude values
-
+    Class to take two oscillators in different planes (treat as uncoupled)
+    and mix amplitude values.
     """
 
     def __init__(self, xTF1, yTF1):
         self.xTF1 = xTF1
         self.yTF1 = yTF1
         self.samples = np.linspace(0.005, 0.1, 159)
-        self.Z=[]
-
-
+        self.Z = []
 
     def mix(self):
         """
-         create a grid to map on 2-D apply basic averaging of oscillators
-         with no interaction assumptions
-        :return: 
+        Create a grid to map on 2-D and apply basic averaging of oscillators
+        with no interaction assumptions.
         """
-
         self.xAxis = self.samples
         self.yAxis = self.samples
 
-        self.X,self.Y=np.meshgrid(self.xAxis,self.yAxis)
+        self.X, self.Y = np.meshgrid(self.xAxis, self.yAxis)
 
-        for x in range(0,len(self.xAxis),1):
-            for y in range(0,len(self.yAxis),1):
+        for x in range(0, len(self.xAxis), 1):
+            for y in range(0, len(self.yAxis), 1):
+                self.Z.append(np.sqrt(self.xTF1[x] ** 2 + self.yTF1[y] ** 2))
+        self.Z = np.array(self.Z).reshape(len(self.xAxis), len(self.xAxis))
 
-                self.Z.append(np.sqrt(self.xTF1[x]**2+self.yTF1[y]**2))
-        self.Z=np.array(self.Z).reshape(len(self.xAxis),len(self.xAxis))
-
-
-    def plotmix(self,n):
+    def plotmix(self, n):
         """
-        :param n: plot number 
-        :return:
+        Plot the mixed oscillator output.
         """
         plot2 = plt.figure(n)
-        fig,ax = plt.subplots()
+        fig, ax = plt.subplots()
         origin = 'lower'
-        CS=ax.contourf(self.xAxis ,self.xAxis , self.Z,100, cmap=plt.cm.jet, origin=origin)
+        CS = ax.contourf(self.xAxis, self.xAxis, self.Z, 100, cmap=plt.cm.jet, origin=origin)
         ax.set_title('Uncoupled Dual Optical Oscillators')
 
     def plotmix_plotly(self):
-
-        fig=go.Figure(data=
-                      go.Contour(z=self.Z,x=self.xAxis, y=self.yAxis,
-                                 contours=dict(start=0.1, end=1.2, size=0.01),
-                                 contours_coloring='heatmap',line_smoothing=0.85, colorscale='jet'))
-        fig.write_html("./uncoupled_oscillator.html",  auto_open=True)
-
+        """
+        Plot the mixed oscillator output using plotly.
+        """
+        fig = go.Figure(data=
+                        go.Contour(z=self.Z, x=self.xAxis, y=self.yAxis,
+                                   contours=dict(start=0.1, end=1.2, size=0.01),
+                                   contours_coloring='heatmap', line_smoothing=0.85, colorscale='jet'))
+        fig.write_html("./uncoupled_oscillator.html", auto_open=True)
 
 
 if __name__ == "__main__":
-
-
-    # example of a an a single oscillator plotting absolute, real and complex components
-    meshConfig1 = Oscillator(a1=0.001,A1=0.01, g=24,l0=32)
-    meshConfig1.config=True
+    # Example of a single oscillator plotting absolute, real and complex components
+    meshConfig1 = Oscillator(a1=0.001, A1=0.01, g=24, l0=32)
+    meshConfig1.config = True
     meshConfig1.filteroutput()
     meshConfig1.OscillatorPhaseGroupDelayDispersion()
     plot1 = plt.figure(1)
@@ -159,38 +121,33 @@ if __name__ == "__main__":
     plt.grid('True')
     plt.xlabel(r'$\omega$')
 
-    # plotting phase
+    # Plotting phase
     plot2 = plt.figure(2)
-    plt.plot(meshConfig1.w, meshConfig1.phi, label='phase')
+    plt.plot(meshConfig1.w, meshConfig1.phi, label='Phase')
     plt.legend(loc="upper right")
     plt.xlim([0, 0.1])
     plt.ylim([-100, 100])
 
-
     plot3 = plt.figure(3)
-    plt.plot(meshConfig1.w, meshConfig1.group_delay, label='group delay')
+    plt.plot(meshConfig1.w, meshConfig1.group_delay, label='Group Delay')
     plt.legend(loc="upper right")
 
-    # plotting groupdelay
+    # Plotting dispersion relation
     plot4 = plt.figure(4)
     plt.plot(meshConfig1.w, meshConfig1.dispersion, label='Dispersion Relation')
     plt.legend(loc="upper right")
 
-
-
-    #second oscillator
-
+    # Second oscillator
     meshConfig2 = Oscillator(a1=0.01, A1=3, g=10, l0=15)
     meshConfig1.config = False
     meshConfig2.filteroutput()
     meshConfig2.OscillatorPhaseGroupDelayDispersion()
 
-    # mixing oscillators with no coupling
-    mix_uncoupled_oscillators = MixingOscillators(meshConfig1.absTF1,meshConfig2.absTF1)
+    # Mixing oscillators with no coupling
+    mix_uncoupled_oscillators = MixingOscillators(meshConfig1.absTF1, meshConfig2.absTF1)
     mix_uncoupled_oscillators.mix()
     mix_uncoupled_oscillators.plotmix(n=10)
     plt.show()
 
-    # plotly/dash plots [needs updating]
+    # Plotly/Dash plots [needs updating]
     mix_uncoupled_oscillators.plotmix_plotly()
-
